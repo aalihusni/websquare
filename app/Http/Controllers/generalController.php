f45f84030dce103ee7a45198edba7a06dd0ff8fb
Ali Husni <ahmad.ali@ajobthing.my>
2018-12-07 20:42:18 +0800

    task backend

--- app/Http/Controllers/generalController.php
+++ app/Http/Controllers/generalController.php
@@ -9,4 +9,17 @@ class generalController extends Controller
     public function test(){
         return view('test');
     }
+
+    public function callApi(Request $request){
+        $input = $request->request->all();
+        $input = $input['input'];
+        $array = explode(' ',$input);
+        $cat_array = [];
+        foreach($array as $cat) {
+            if (preg_match('/\w(cat)$\b/', $cat)) {
+                $cat_array[] = $cat;
+            }
+        }
+        return response(json_encode($cat_array));
+    }
 }
